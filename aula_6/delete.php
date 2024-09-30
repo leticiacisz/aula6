2dcc38600ad39645410a4742eab944ba3e3245d3
VeeAxt <vinicius_axt@estudante.sesisenai.org.br>
2024-09-30 09:36:03 -0300

    criei delete

---
+++ aula_6/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'db.php';
+
+$id_conteudo = $_GET['id_conteudo']
+
+$sql = "DELETE FROM user WHERE id_conteudo=$id_conteudo";
+
+if ($conteudo_nota->query($sql) === TRUE) {
+    echo "Registro excluído com sucesso";
+} else {
+    echo "Erro: " . $id_conteudo . "<br>" . $conteudo_nota->error;
+}
+
+$conn -> close();
+
+header ("Location: read.php");
+exit();
+
+?>
