5ba9e2fb7681fe6ab2d77ed1aeda285ea7bebcb2
VeeAxt <vinicius_axt@estudante.sesisenai.org.br>
2024-09-30 09:44:12 -0300

    arrumei o delete

--- aula_6/delete.php
+++ aula_6/delete.php
@@ -4,7 +4,7 @@ include 'db.php';
 
 $id_conteudo = $_GET['id_conteudo']
 
-$sql = "DELETE FROM user WHERE id_conteudo=$id_conteudo";
+$sql = "DELETE FROM conteudo WHERE id_conteudo=$id_conteudo";
 
 if ($conteudo_nota->query($sql) === TRUE) {
     echo "Registro excluído com sucesso";
