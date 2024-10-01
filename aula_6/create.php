47db5cd9157dbfc69142d8235c878923f3a5ef27
leticia_cisz <leticia_cisz@estudante.sesisenai.org.br>
2024-10-01 08:15:05 -0300

    arrumei o create

--- aula_6/create.php
+++ aula_6/create.php
@@ -2,20 +2,20 @@
 include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id_conteudo = $_POST['id_conteudo'];
-    $nome_conteudo = $_POST['nome_conteudo'];
-    $conteudo_nota = $_POST['conteudo_nota'];
+    $id_conteudo = $_POST['id_conteudo'] ?? null;
+    $nome_conteudo = $_POST['nome_conteudo'] ?? null;
+    $conteudo_nota = $_POST['conteudo_nota'] ?? null;
 
-    $sql = "INSERT INTO user (id_conteudo, nome_conteudo, conteudo_nota) VALUES ('$id_conteudo', '$nome_conteudo', '$conteudo_nota')";
+    $sql = "INSERT INTO conteudo (id_conteudo, nome_conteudo, conteudo_nota) VALUES ('$id_conteudo', '$nome_conteudo', '$conteudo_nota')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Novo registro criado com sucesso";
     } else {
         echo "Erro: " . $sql . "<br>" . $conn->error;
     }
-}
 
 $conn->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +29,7 @@ $conn->close();
    <form method="post" action="create.php">
     <h1>Criar Notas:</h1>
     Nome do Conteudo: <input type="text" name="nome_conteudo" required><br><br>
-    Conteudo Nota: <input type="text" name="conteudo-nota" required><br><br>
+    Conteudo Nota: <input type="text" name="conteudo_nota" required><br><br>
     <br>
     <input type="submit" value="Adicionar nota" class="adicionar">
     <button type="button" onclick="window.location.href='read.php';" class="registros">Ver registros</button>  
